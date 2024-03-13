703c66ddc8d438cee7f729bde015e58071bb0563
LautaroLinari <lautarolinari18@hotmail.com>
2024-03-12 21:13:29 -0300

    error tipografy

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import NavLink from './Navlink.jsx';
+import NavLink from './NavLink.jsx';
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from './MenuOverlay.jsx';
 
